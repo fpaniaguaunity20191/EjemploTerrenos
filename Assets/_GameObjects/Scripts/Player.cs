bb80d88f28194066e93136a05b6971b8471507d4
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-01-23 13:58:49 +0100

    2019-01-23

---
+++ Assets/_GameObjects/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject linterna;
+    [SerializeField] AudioSource audioSource;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L)){
+            linterna.SetActive(!linterna.activeSelf);
+        }
+        if (Input.GetButtonDown("Fire1")) {
+            audioSource.Play();
+        }
+    }
+}
